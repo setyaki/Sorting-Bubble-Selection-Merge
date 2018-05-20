87fa53df58363958cd101f236c916220cc967b3f
setyaki <34120275+setyaki@users.noreply.github.com>
2018-05-20 14:37:18 +0700

    Add files via upload

---
+++ sort aja.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main () {
+	
+	int A[7]={3,4,1,5,6,8,2};
+	cout<<"Data Input :";
+	for (int i=0;i<7;i++){
+		cout<<A[i]<<" ";
+	}
+	cout<<endl;
+	cout<<"Swapping :";
+	for (int j=0;j<7;j++){
+		for (int k=0;k<6;k++){
+			
+			if (A[k]>A[k+1]){
+				cout<<A[k];
+				swap (A[k], A[k+1]);
+				cout<<A[k]<<" ";
+			}
+		}
+	}
+	cout<<endl<<"Sorting Result :";
+	for (int l=0;l<7;l++){
+		cout<<A[l]<<" ";
+	}
+	
+}
