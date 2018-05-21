0d011dae2922ba997aede613d472eaf71e2cb113
1717051047-Fikri Aulia <38536861+fikriaulia23@users.noreply.github.com>
2018-05-21 21:11:46 +0700

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -8,23 +8,23 @@ int main () {
 		cout<<A[i]<<" ";
 	}
 	cout<<endl;
-	cout<<"Swapping :";
-	for (int j=0;j<7;j++){
-		for (int k=0;k<6;k++){
+	cout<<" Swapping :";
+	for (int j=0; j<7; j++){
+		for (int k=0; k<6; k++){
 			
-			if (A[k]>A[k+1]){
+			if (A[k] > A[k+1]){
 				cout<<"("<<A[k]<<",";
 				swap (A[k], A[k+1]);
 				cout<<A[k]<<") ";
 			}
 		}
 	}
-	cout<<endl<<"Ascending Sorting Result :";
-	for (int l=0;l<7;l++){
+	cout<<endl<<" Ascending Sorting Result :";
+	for (int l=0; l<7; l++){
 		cout<<A[l]<<" ";
 	}
-	cout<<endl<<"Descending Sorting Result :";
-	for (int p=6;p>=0;p--){
+	cout<<endl<<" Descending Sorting Result :";
+	for (int p=6; p>=0; p--){
 		cout<<A[p]<<" ";
 	}
 	
